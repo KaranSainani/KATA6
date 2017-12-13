4eb97477bd4edb1503aa8a195bdb60e0831d920e
karansainani <karansainani@192.168.1.34>
2017-12-13 22:05:14 +0000

    Tercera versión de kata6

--- src/kata6/controller/Kata6.java
+++ src/kata6/controller/Kata6.java
@@ -1,9 +1,12 @@
 package kata6.controller;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.List;
 import kata6.model.Histogram;
 import kata6.model.Mail;
+import kata6.model.Person;
+import kata6.view.DataBaseList;
 import kata6.view.HistogramDisplay;
 import kata6.view.MailHistogramBuilder;
 import kata6.view.MailListReader;
@@ -15,6 +18,8 @@ import kata6.view.MailListReader;
     private MailHistogramBuilder<Mail> builder;
     private Histogram<String> domains;
     private Histogram<Character> letters;
+    private Histogram<Character> gender;
+    private  List<Person> people;
    
     public static void main(String[] args) throws IOException {
       Kata6 kata6 = new Kata6();
@@ -33,7 +38,7 @@ import kata6.view.MailListReader;
         builder = new MailHistogramBuilder<Mail>(mailList);
     }
     
-    private void process(){
+    private void process() throws ClassNotFoundException, SQLException{
         domains = builder.build(new Attribute<Mail, String>() {
         
         @Override
@@ -50,10 +55,21 @@ import kata6.view.MailListReader;
         }
     });
     
+    people = DataBaseList.read();
+    MailHistogramBuilder<Person> builderPerson = new MailHistogramBuilder<>(people);
+    gender = builderPerson.build(new Attribute<Person,Character>() {
+        
+        @Override
+        public Character get(Person item) {
+            return item.getGender();
+        }
+    });
+    
     
     private void output() {
         new HistogramDisplay(domains, "Dominios").execute();
         new HistogramDisplay (letters,"Primer Caracter").execute();
+        new HistogramDisplay (gender,"Gender").execute();
         histoDisplay.execute();
     }
 }
